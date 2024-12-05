dd17b7d9b34bdb222332b4c7c8ed60b3f3a1992d
DJVolin <danieljvolin@gmail.com>
2024-12-05 21:02:18 +0000

    Added café website code

---
+++ config.js
@@ -0,0 +1,4 @@
+window.COFFEE_CONFIG = {
+	API_GW_BASE_URL_STR: "https://a7zk9binsa.execute-api.us-east-1.amazonaws.com/prod",
+	COGNITO_LOGIN_BASE_URL_STR: "https://cc225430.auth.us-east-1.amazoncognito.com/login?client_id=6r2iafbt7si62a879gnn1pj5dd&response_type=token&scope=email+openid&redirect_uri=https://d3szrngwnsq0wl.cloudfront.net/callback.html"
+};
